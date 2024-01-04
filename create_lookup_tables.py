14be9ef7b612f2dfdba2760916518227d1d922d9
reuss <torsten.reuss@nokia.com>
2024-01-04 13:03:46 +0100

    Added enum check for boolean fields.

--- create_lookup_tables.py
+++ create_lookup_tables.py
@@ -19,36 +19,45 @@ def create_lookup_tables(config, nenameparam="NETWORK_ELEMENT_NAME"):
     ''' Create the lookup Tables '''
     prod = config.factoryProductName
     trans = config.transaction
+
+    # LKT_TND_FACTORY_PRODUCT_PARAMETERS
     lkt1 = LookupTable('LKT_TND_FACTORY_PRODUCT_PARAMETERS')
     lkt1.add(LtEntry(prod+"#"+trans, joinparams(config.input_params)))
 
-    lkt2 = LookupTable.from_validations(prod, trans, *config.cramer_validations)
+    # LKT_MANDATORY_PARAM_CHECK
+    lkt2 = LookupTable('LKT_MANDATORY_PARAM_CHECK')
+    lkt2.add(LtEntry('FACTORY_PRODUCT_'+prod+"#"+trans, " ".join(p.name for p in config.input_params if p.mandatory)+" ", "ORDER_TYPE IL_REQ_GROUP ORDER_EXTERNAL_ORDER_ID "))
 
-    lkt3 = LookupTable('LKT_TND_CRAMER_SUBORDERS')
-    lkt3.add(LtEntry(prod+"#"+trans+"#SUBORDER_PRODUCT", "TECHPROD_CRAMER_FACT_PROD_"+prod))
-    lkt3.add(LtEntry(prod+"#"+trans+"#PARAMETERS", joinparams([p for p in config.input_params if not p.special])))
-    lkt3.add(LtEntry(prod+"#"+trans+"#RETURN_PARAMETERS", joinparams(config.cramer_output_params)))
-    lkt3.add(LtEntry(prod+"#"+trans+"#GE_PARAMETERS", joinparams([p for p in config.input_params if p.cramerStorage == config.factoryProductName+"_GE"])))
+    # LKT_TND_ENUM_PARAM_CHECK
+    lkt3 = LookupTable('LKT_TND_ENUM_PARAM_CHECK')
+    lkt3.add(LtEntry(prod+"#"+trans+"#ENUM_PARAMS", ";".join(p.name for p in config.input_params if p.valuetype == "enumerated" or p.valuetype == "boolean")+";"))
+    for p in config.input_params:
+        if p.valuetype == "enumerated":
+            lkt3.add(LtEntry(prod+"#"+trans+"#{}".format(p.name), ";".join(p.enumvalues)+";"))
+        elif p.valuetype == "boolean":
+            lkt3.add(LtEntry(prod+"#"+trans+"#{}".format(p.name), "true;false;"))
 
-    lkt4 = LookupTable('LKT_TND_STABLENET')
+    # LKT_TND_CRAMER_COMMAND_VALIDATION
+    lkt4 = LookupTable.from_validations(prod, trans, *config.cramer_validations)
+
+    # LKT_TND_CRAMER_SUBORDERS
+    lkt5 = LookupTable('LKT_TND_CRAMER_SUBORDERS')
+    lkt5.add(LtEntry(prod+"#"+trans+"#SUBORDER_PRODUCT", "TECHPROD_CRAMER_FACT_PROD_"+prod))
+    lkt5.add(LtEntry(prod+"#"+trans+"#PARAMETERS", joinparams([p for p in config.input_params if not p.special])))
+    lkt5.add(LtEntry(prod+"#"+trans+"#RETURN_PARAMETERS", joinparams(config.cramer_output_params)))
+    lkt5.add(LtEntry(prod+"#"+trans+"#GE_PARAMETERS", joinparams([p for p in config.input_params if p.cramerStorage == config.factoryProductName+"_GE"])))
+
+    # LKT_TND_STABLENET
+    lkt6 = LookupTable('LKT_TND_STABLENET')
     if config.transaction == "Create":
-        lkt4.add(LtEntry(prod+"#"+trans+"#ORDER_DESCRIPTION", "new_rfs"))
+        lkt6.add(LtEntry(prod+"#"+trans+"#ORDER_DESCRIPTION", "new_rfs"))
     elif config.transaction == "Delete":
-        lkt4.add(LtEntry(prod+"#"+trans+"#ORDER_DESCRIPTION", "cease_rfs"))
+        lkt6.add(LtEntry(prod+"#"+trans+"#ORDER_DESCRIPTION", "cease_rfs"))
     else:
         raise ValueError("Action {} not (yet) supported for Stablenet Requests.")
-    lkt4.add(LtEntry(prod+"#"+trans+"#TARGET_DEVICE", nenameparam))
-    lkt4.add(LtEntry(prod+"#"+trans+"#RFS_NAME", config.factoryProductName+"_RFS_NAME"))
-    lkt4.add(LtEntry(prod+"#"+trans+"#PARAMETERS", joinparams([p for p in config.input_params if not p.special], config.cramer_output_params)))
-
-    lkt5 = LookupTable('LKT_TND_ENUM_PARAM_CHECK')
-    lkt5.add(LtEntry(prod+"#"+trans+"#ENUM_PARAMS", ";".join(p.name for p in config.input_params if p.valuetype == "enumerated")+";"))
-    for p in config.input_params:
-        if p.valuetype == "enumerated" or p.valuetype == "enumeration":
-            lkt5.add(LtEntry(prod+"#"+trans+"#{}".format(p.name), ";".join(p.enumvalues)+";"))
-
-    lkt6 = LookupTable('LKT_MANDATORY_PARAM_CHECK')
-    lkt6.add(LtEntry('FACTORY_PRODUCT_'+prod+"#"+trans, " ".join(p.name for p in config.input_params if p.mandatory)+" ", "ORDER_TYPE IL_REQ_GROUP ORDER_EXTERNAL_ORDER_ID "))
+    lkt6.add(LtEntry(prod+"#"+trans+"#TARGET_DEVICE", nenameparam))
+    lkt6.add(LtEntry(prod+"#"+trans+"#RFS_NAME", config.factoryProductName+"_RFS_NAME"))
+    lkt6.add(LtEntry(prod+"#"+trans+"#PARAMETERS", joinparams([p for p in config.input_params if not p.special], config.cramer_output_params)))
 
     return (lkt1, lkt2, lkt3, lkt4, lkt5, lkt6)
 
