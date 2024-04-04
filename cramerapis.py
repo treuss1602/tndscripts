1756be579aa274b5885a228b308e2982952547d3
reuss <torsten.reuss@nokia.com>
2024-04-04 17:35:22 +0200

    PHY_ESILAG

--- cramerapis.py
+++ cramerapis.py
@@ -3,6 +3,7 @@
 #              Factory Product      Query API                  Identify API (if applicable)
 CramerAPIs = { "PHY_SINGLE_LINK": ("getPHYSingleLinkDetails", "identifyPHYSingleLink"),
                "PHY_ILAG":        ("getPHYILAGDetails",       "identifyPHYILAG"),
+               "PHY_ESILAG":      ("getPHYESILAGDetails",     "identifyPHYESILAG"),
                "IPVPN_CORE":      ("getIPVPNCoreDetails",     "identifyIPVPNCore"),
                "IPVPN_SAP":       ("getIPVPNSAPDetails",      "identifyIPVPNSap"),
                "ELAN_CORE":       ("getELANCoreDetails",      "identifyELANCore"),
